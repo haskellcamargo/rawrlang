bf31afb18af79893b3ce2733b79fafe7fd2c215c
Haskell Camargo <marcelocamargo@linuxmail.org>
2015-05-14 21:57:25 -0300

    Deprecated parser

---
+++ src/Grammar.hs
@@ -0,0 +1,5 @@
+{--
+  EBNF Grammar for RawrLang.
+  I should have written this BEFORE writing the parser. Now, take up all the
+  consequences, you dumb Celão!
+--}
--- src/Token.o.php
+++
@@ -1,7 +0,0 @@
-object(RawrLang\Lexer\Token)#4 (2) {
-  ["name"]=>
-  string(5) "T_EOF"
-  ["value"]=>
-  int(0)
-}
-Error. Unexpected T_EOF
